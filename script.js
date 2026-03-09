9131fca4fc0e5c355f023ac5990f9a3d757039e1
Saidul Islam <youremail@example.com>
2026-03-09 16:24:25 +0600

    cmt

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+// Global State
+
+let allIssues = [];
+let currentTab = "All";
+
+// Login page
+
+const loginForm = document.getElementById("loginForm");
+if (loginForm) {
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+
+    if (user === "admin" && pass === "admin123") {
+      document.getElementById("loginPage").classList.add("hidden");
+      const dashboard = document.getElementById("dashboardPage");
+      dashboard.classList.remove("hidden");
+      dashboard.classList.add("flex");
+      loadInitialData();
+    } else {
+      alert("Invalid Credentials! Hint: admin / admin123");
+    }
+  });
+}
+
